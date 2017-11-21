0e2948d3d337fb30ed743fad56fd76534e70f8ac
Elias Mason <eliasjmason@gmail.com>
2017-11-21 10:14:55 -0800

    Remove excess import

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { TopNav } from './top-nav';
-import { RESTART_GAME, GENERATE_AURAL_UPDATE, generateAuralUpdate } from '../actions';
+import { RESTART_GAME, GENERATE_AURAL_UPDATE } from '../actions';
 
 describe('<TopNav />', () => {
   it('Renders without crashing', () => {
