4add40a99eb4e908af1a1e843fa089e4d8e4e741
Joe Turner <joe@oampo.co.uk>
2017-11-28 12:52:05 +0000

    Update tests for correctAnswer in action

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 
-import { TopNav } from './top-nav';
-import { RESTART_GAME, GENERATE_AURAL_UPDATE } from '../actions';
+import {TopNav} from './top-nav';
+import {RESTART_GAME, GENERATE_AURAL_UPDATE} from '../actions';
 
 describe('<TopNav />', () => {
-  it('Renders without crashing', () => {
-    shallow(<TopNav />);
-  });
+    it('Renders without crashing', () => {
+        shallow(<TopNav />);
+    });
 
-  it('Should dispatch restartGame when new game is clicked', () => {
-    const dispatch = jest.fn();
-    const wrapper = shallow(<TopNav dispatch={dispatch} />);
-    const link = wrapper.find('.new');
-    link.simulate('click');
-    expect(dispatch).toHaveBeenCalled();
-    expect(dispatch.mock.calls[0][0].type).toEqual(RESTART_GAME);
-  });
+    it('Should dispatch restartGame when new game is clicked', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(<TopNav dispatch={dispatch} />);
+        const link = wrapper.find('.new');
+        link.simulate('click');
+        expect(dispatch).toHaveBeenCalled();
+        const action = dispatch.mock.calls[0][0];
+        expect(action.type).toEqual(RESTART_GAME);
+        expect(action.correctAnswer).toBeGreaterThanOrEqual(0);
+        expect(action.correctAnswer).toBeLessThanOrEqual(100);
+    });
 
-  it('Should dispatch generateAuralUpdate when new game is clicked', () => {
-    const dispatch = jest.fn();
-    const wrapper = shallow(<TopNav dispatch={dispatch} />);
-    const link = wrapper.find('.status-link');
-    link.simulate('click');
-    expect(dispatch).toHaveBeenCalled();
-    expect(dispatch.mock.calls[0][0].type).toEqual(GENERATE_AURAL_UPDATE);
-  });
+    it('Should dispatch generateAuralUpdate when new game is clicked', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(<TopNav dispatch={dispatch} />);
+        const link = wrapper.find('.status-link');
+        link.simulate('click');
+        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch.mock.calls[0][0].type).toEqual(GENERATE_AURAL_UPDATE);
+    });
 });
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,80 +1,81 @@
 import reducer from './reducer';
-import { restartGame, makeGuess, generateAuralUpdate } from './actions';
+import {restartGame, makeGuess, generateAuralUpdate} from './actions';
 
 describe('Reducer', () => {
-  it('Should set the initial state when nothing is passed in', () => {
-    const state = reducer(undefined, { type: '__UNKNOWN' });
+    it('Should set the initial state when nothing is passed in', () => {
+        const state = reducer(undefined, {type: '__UNKNOWN'});
 
-    expect(state.guesses).toEqual([]);
-    expect(state.feedback).toEqual('Make your guess!');
-    expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
-    expect(state.correctAnswer).toBeLessThanOrEqual(100);
-    expect(state.auralStatus).toEqual('');
-  });
-
-  it('Should return the current state on an unknown action', () => {
-    let currentState = {};
-    const state = reducer(currentState, { type: '__UNKNOWN' });
-    expect(state).toBe(currentState);
-  });
-
-  describe('restartGame', () => {
-    it('Should start a new game', () => {
-      // Mess up the state a bit to simulate an existing game
-      let state = {
-        guesses: [1, 2, 3, 4],
-        feedback: 'Awesome',
-        correctAnswer: -1 // Negative so different to new game
-      };
-      state = reducer(state, restartGame());
-      expect(state.guesses).toEqual([]);
-      expect(state.feedback).toEqual('Make your guess!');
-      expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
-      expect(state.correctAnswer).toBeLessThanOrEqual(100);
-      expect(state.auralStatus).toEqual('');
+        expect(state.guesses).toEqual([]);
+        expect(state.feedback).toEqual('Make your guess!');
+        expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
+        expect(state.correctAnswer).toBeLessThanOrEqual(100);
+        expect(state.auralStatus).toEqual('');
     });
-  });
 
-  describe('makeGuess', () => {
-    it('Should make a guess', () => {
-      // Fix the correct answer so we know what we're aiming for
-      let state = {
-        guesses: [],
-        feedback: '',
-        correctAnswer: 100 // Negative so different to new game
-      };
-
-      state = reducer(state, makeGuess(25));
-      expect(state.guesses).toEqual([25]);
-      expect(state.feedback).toEqual('You\'re Ice Cold...');
-
-      state = reducer(state, makeGuess(60));
-      expect(state.guesses).toEqual([25, 60]);
-      expect(state.feedback).toEqual('You\'re Cold...');
-
-      state = reducer(state, makeGuess(80));
-      expect(state.guesses).toEqual([25, 60, 80]);
-      expect(state.feedback).toEqual('You\'re Warm.');
-
-      state = reducer(state, makeGuess(95));
-      expect(state.guesses).toEqual([25, 60, 80, 95]);
-      expect(state.feedback).toEqual('You\'re Hot!');
-
-      state = reducer(state, makeGuess(100));
-      expect(state.guesses).toEqual([25, 60, 80, 95, 100]);
-      expect(state.feedback).toEqual('You got it!');
+    it('Should return the current state on an unknown action', () => {
+        let currentState = {};
+        const state = reducer(currentState, {type: '__UNKNOWN'});
+        expect(state).toBe(currentState);
     });
-  });
 
-  it('Can generate aural updates', () => {
-    let state = {
-      guesses: [25, 3, 90],
-      feedback: 'You\'re Warm.',
-      auralStatus: ''
-    };
-    
-    state = reducer(state, generateAuralUpdate());
-    expect(state.auralStatus).toEqual('Here\'s the status of the game right now: You\'re Warm. You\'ve made 3 guesses. In order of most- to least-recent, they are: 90, 3, 25');
+    describe('restartGame', () => {
+        it('Should start a new game', () => {
+            // Mess up the state a bit to simulate an existing game
+            let state = {
+                guesses: [1, 2, 3, 4],
+                feedback: 'Awesome',
+                correctAnswer: 4
+            };
+            const correctAnswer = 10;
+            state = reducer(state, restartGame(correctAnswer));
+            expect(state.guesses).toEqual([]);
+            expect(state.feedback).toEqual('Make your guess!');
+            expect(state.correctAnswer).toEqual(correctAnswer);
+            expect(state.auralStatus).toEqual('');
+        });
+    });
 
-  });
+    describe('makeGuess', () => {
+        it('Should make a guess', () => {
+            // Fix the correct answer so we know what we're aiming for
+            let state = {
+                guesses: [],
+                feedback: '',
+                correctAnswer: 100 // Negative so different to new game
+            };
+
+            state = reducer(state, makeGuess(25));
+            expect(state.guesses).toEqual([25]);
+            expect(state.feedback).toEqual("You're Ice Cold...");
+
+            state = reducer(state, makeGuess(60));
+            expect(state.guesses).toEqual([25, 60]);
+            expect(state.feedback).toEqual("You're Cold...");
+
+            state = reducer(state, makeGuess(80));
+            expect(state.guesses).toEqual([25, 60, 80]);
+            expect(state.feedback).toEqual("You're Warm.");
+
+            state = reducer(state, makeGuess(95));
+            expect(state.guesses).toEqual([25, 60, 80, 95]);
+            expect(state.feedback).toEqual("You're Hot!");
+
+            state = reducer(state, makeGuess(100));
+            expect(state.guesses).toEqual([25, 60, 80, 95, 100]);
+            expect(state.feedback).toEqual('You got it!');
+        });
+    });
+
+    it('Can generate aural updates', () => {
+        let state = {
+            guesses: [25, 3, 90],
+            feedback: "You're Warm.",
+            auralStatus: ''
+        };
+
+        state = reducer(state, generateAuralUpdate());
+        expect(state.auralStatus).toEqual(
+            "Here's the status of the game right now: You're Warm. You've made 3 guesses. In order of most- to least-recent, they are: 90, 3, 25"
+        );
+    });
 });
